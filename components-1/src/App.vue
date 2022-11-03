3a437915c08ca5632176520e365d364e77766b17
vernikus <jonatahn5303@gmail.com>
2022-11-03 14:38:13 -0600

    'test'

--- components-1/src/App.vue
+++ components-1/src/App.vue
@@ -1,6 +1,19 @@
-<script setup>
-
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const counter = ref(0);
+    const increment = () =>{
+      counter.value++
+    }
+    return {
+      counter,increment
+    }
+  },
+};
 </script>
+
 <template>
   <h1>Hello world</h1>
+  <button @click="increment">{{counter}}</button>
 </template>
