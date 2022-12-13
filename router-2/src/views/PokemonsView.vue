6c1c81319ea9235c96fb805b08a7c4ad040ef39d
vernikus <jonatahn5303@gmail.com>
2022-12-13 01:00:02 -0600

    Se agrego un composable optimizando el codigo

--- router-2/src/views/PokemonsView.vue
+++ router-2/src/views/PokemonsView.vue
@@ -1,21 +1,23 @@
 <script setup>
-import axios from "axios";
-import { ref } from "vue";
+// import axios from "axios";
+// import { ref } from "vue";
 import {RouterLink} from 'vue-router'
+import {useGetData} from '@/composables/getData.js'
 
-const pokemons = ref("[]");
+// const pokemons = ref("[]");
 
-const getData = async () => {
-  try {
-    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon"); //destructuramos data, se crea una constante, se tiene que tener invocar a la propiedad del objeto, es decir, el nombre especifico de la propiedad a la que se quiere acceder
-    // console.log(data);
-    // console.log(data.results);
-    pokemons.value = data.results //igualamos los pokemons al array
-  } catch (err) {
-    console.log(err);
-  }
-};
-getData();
+const {data,getData,loading,errorData} = useGetData()
+// const getData = async () => {
+//   try {
+//     const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon"); //destructuramos data, se crea una constante, se tiene que tener invocar a la propiedad del objeto, es decir, el nombre especifico de la propiedad a la que se quiere acceder
+//     // console.log(data);
+//     // console.log(data.results);
+//     pokemons.value = data.results //igualamos los pokemons al array
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+getData("https://pokeapi.co/api/v2/pokemon");
 
 </script>
 
@@ -23,15 +25,21 @@ getData();
   <main>
     <section>
       <h1>Pokemons</h1>
-      <ul>
-        <li 
-        v-for="pokemon in pokemons"
-        :key='pokemon.name'
-        
-        >
-        <RouterLink :to="`/pokemons/${pokemon.name}`">{{pokemon.name}}</RouterLink> <!--Se convierten a rutas dinamicas todos los resultados conel RouterLink-->
-        </li>
-      </ul>
+      <p v-if="loading">Loading information ....</p>
+      <div class="alert alert-danger" v-if="errorData">{{errorData}}</div> <!--Asi es como se pueden pintar los errores, se manejan en el composable-->
+      <div v-if="data">
+        <ul>
+          <li 
+          v-for="pokemon in data.results"
+          :key='pokemon.name'
+          
+          >
+          <RouterLink :to="`/pokemons/${pokemon.name}`">{{pokemon.name}}</RouterLink> <!--Se convierten a rutas dinamicas todos los resultados conel RouterLink-->
+          </li>
+        </ul>
+        <button :disabled="!data.previous" class="btn btn-warning me-2" @click="getData(data.previous)">Previus</button>
+        <button :disabled="!data.next" class="btn btn-primary" @click="getData(data.next)">Next</button>
+      </div>
     </section>
   </main>
 </template>
