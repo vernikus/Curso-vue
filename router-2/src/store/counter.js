c4f5c6a551a28582d67cfea1216d969eb657e4ee
vernikus <jonatahn5303@gmail.com>
2022-12-14 16:24:28 -0600

    first commit

---
+++ router-2/src/store/counter.js
@@ -0,0 +1,5 @@
+import {defineStore} from 'piniea'
+
+export const useCounterStore = defineStore('counter',{ //como primer argumento recibe un identificador por cada uno de los archivos que estamos modularizando
+
+})
