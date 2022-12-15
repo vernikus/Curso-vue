b24517e2ed0548618f9cfdcf5f4172dcc5f40366
vernikus <jonatahn5303@gmail.com>
2022-12-14 19:52:00 -0600

    Finally counter

--- router-2/src/store/counter.js
+++ router-2/src/store/counter.js
@@ -1,5 +1,27 @@
-import {defineStore} from 'piniea'
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useCounterStore = defineStore('counter',{ //como primer argumento recibe un identificador por cada uno de los archivos que estamos modularizando
-
-})
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const increment = () => count.value++;
+  const double = computed(() => count.value * 2);
+  return {
+    count,
+    increment,
+    double,
+  };
+});
+// export const useCounterStore = defineStore('counter',{ //como primer argumento recibe un identificador por cada uno de los archivos que estamos modularizando
+//     state: ()=> ({
+//         count:0
+//     }),
+//     actions:{ //dentro de las acciones podemos tener los diferentes metodos o funciones que se requieren
+//         increment(){
+//             this.count ++
+//         }
+//     },
+//     getters:{ // los getters son las propiedades computadas
+//         double: (state) => state.count * 2
+//         // siempre se tiene que recibir el state, por esa razon se puede acceder directamente a sua propiedades
+//     }
+// })
--- router-2/src/views/HomeView.vue
+++ router-2/src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from "@/store/counter.js";
+import { storeToRefs } from "pinia";
+
+const useCounter = useCounterStore();
+const { increment } = useCounter;
+const { count, double } = storeToRefs(useCounter); //Las referencias reactivas y los getters, se transforman en datos 'reactive', estos datos no pueden ser destructurados directamente, se tiene que usar el metodo 'storeToRefs' importado directamente de pinia, y así se puede hacer la destructuracion si problemas
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home counter : {{ count }}</h1>
+    <h2>Double : {{ double }}</h2>
+    <button @click="increment">Increment</button>
   </main>
 </template>
